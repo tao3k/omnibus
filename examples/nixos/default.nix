fbcf1845e6b6f3b931375a06faa3f8814cc2bbd2
guangtao <gtrunsec@hardenedlinux.org>
2023-09-20 03:07:03 -0700

    refactor: done the minor version

--- examples/nixos/default.nix
+++ examples/nixos/default.nix
@@ -1,18 +1,18 @@
 let
-  pops = lib.mapAttrs (_: v: v.exports) (
-    lib.removeAttrs super.pops [ "inputs__" ]
+  exporter = lib.mapAttrs (_: v: v.exports) (
+    lib.removeAttrs super.pops [ "inputs" ]
   );
 in
 {
   nixosSuites = lib.flatten [
-    pops.selfNixOSProfiles.default.bootstrap
+    exporter.selfNixOSProfiles.default.bootstrap
 
     # self.nixosProfiles.default.presets.boot
-    pops.nixosModules.default.programs.git
+    exporter.nixosModules.default.programs.git
 
     # --custom profiles
-    pops.nixosProfiles.customProfiles.nix
-    pops.nixosProfiles.customProfiles.boot
+    exporter.nixosProfiles.customProfiles.nix
+    exporter.nixosProfiles.customProfiles.boot
 
     (selfLib.mkHome
       {
@@ -29,7 +29,7 @@ in
   ];
 
   homeSuites = [
-    pops.homeProfiles.customProfiles.hyprland
-    # self.homeModules.wayland.windowManager.hyprland
+    exporter.homeProfiles.customProfiles.hyprland
+    # super.pops.homeModules.wayland.windowManager.hyprland
   ];
 }
