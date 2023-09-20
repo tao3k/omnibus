b36ffd621ab88663aebcac2fe1d28bcbbfab4297
guangtao <gtrunsec@hardenedlinux.org>
2023-09-20 03:46:30 -0700

    refactor: done the minor version

--- examples/nixos/pops/inputs.nix
+++ examples/nixos/pops/inputs.nix
@@ -1,4 +1,6 @@
 (POS.loadInputs.addInputsExtender (
-  POP.extendPop flops.flake.pops.inputsExtender (self: super: { })
+  POP.extendPop flops.flake.pops.inputsExtender (
+    self: super: { inputs.nixpkgs = POS.loadInputs.outputs.nixpkgs.legacyPackages; }
+  )
 )).setSystem
   "x86_64-linux"
