aa5f517c3c9915982bb52c00adb12339a47d92ff
guangtao <gtrunsec@hardenedlinux.org>
2023-09-21 00:42:31 -0700

    refactor(nixosModules): update loadModule

--- nixos/nixosModules/boot/systemd-initrd.nix
+++ nixos/nixosModules/boot/systemd-initrd.nix
@@ -3,35 +3,31 @@
     with lib;
     mkMerge [
       (mkIf cfg.__profiles__.systemd-initrd.enable {
-        boot = {
-          loader = {
-            # timeout = 0;
-            efi.canTouchEfiVariables = true;
-            # https://discourse.nixos.org/t/configure-grub-on-efi-system/2926/7
-            grub = {
-              enable = true;
-              efiSupport = true;
-              device = "nodev";
-              useOSProber = true;
-            };
+        loader = {
+          # timeout = 0;
+          efi.canTouchEfiVariables = true;
+          # https://discourse.nixos.org/t/configure-grub-on-efi-system/2926/7
+          grub = {
+            enable = true;
+            efiSupport = true;
+            device = "nodev";
+            useOSProber = true;
           };
-          initrd = {
-            systemd = {
-              enable = true;
-              # emergencyAccess = true;
-            };
+        };
+        initrd = {
+          systemd = {
+            enable = true;
+            # emergencyAccess = true;
           };
         };
       })
     ];
 
-  options =
-    with lib;
-    mkOpt {
-      __profiles__.systemd-initrd.enable = mkOption {
-        type = types.bool;
-        default = false;
-        description = "Enable systemd-initrd as bootloader.";
-      };
+  options = with lib; {
+    __profiles__.systemd-initrd.enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Enable systemd-initrd as bootloader.";
     };
+  };
 }
