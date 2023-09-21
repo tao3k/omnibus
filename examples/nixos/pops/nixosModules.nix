0d94e8c3a4ada3c5fc637f52753dc7921ec07c7b
guangtao <gtrunsec@hardenedlinux.org>
2023-09-21 01:50:02 -0700

    refactor(nixosModules): add dmerge with mkMerge example

--- examples/nixos/pops/nixosModules.nix
+++ examples/nixos/pops/nixosModules.nix
@@ -4,12 +4,9 @@
     (POP.extendPop flops.haumea.pops.exporter (
       self: super: {
         exports.customModules.boot =
+          with dmerge;
           self.outputsForTarget.dmerge
-            {
-              config.loader = {
-                timeout = 10;
-              };
-            }
+            { config.contents = update [ 0 ] [ { content.loader.timeout.content = 10; } ]; }
             [ "boot" ];
       }
     ))
