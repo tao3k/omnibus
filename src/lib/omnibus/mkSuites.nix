7fab3f4c398158ed8eb76382bfef93c5d79fe608
guangtao <gtrunsec@hardenedlinux.org>
2023-12-08 01:17:53 -0800

    feat(lib/omnibus/mkSuites): add meta attr

--- src/lib/omnibus/mkSuites.nix
+++ src/lib/omnibus/mkSuites.nix
@@ -1,5 +1,6 @@
 {lib, super}:
 attrs:
-lib.mapAttrs
-  (_: v: if lib.isList v then lib.flatten (super.concatProfiles v) else v)
-  attrs
+let
+  fun = f: lib.mapAttrs (_: v: if lib.isList v then lib.flatten (f v) else v);
+in
+fun super.concatProfiles attrs // {meta = fun lib.id attrs;}
