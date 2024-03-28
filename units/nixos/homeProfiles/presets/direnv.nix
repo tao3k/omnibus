684eaa6f46c30c18e53875e200180980797f5902
guangtao <gtrunsec@hardenedlinux.org>
2024-03-27 19:44:49 -0700

    fix(homeProfiles.prests.direnv): remove fishIntegation

--- units/nixos/homeProfiles/presets/direnv.nix
+++ units/nixos/homeProfiles/presets/direnv.nix
@@ -11,7 +11,6 @@ in
   programs.direnv = {
     enable = true;
     enableZshIntegration = lib.mkIf config.programs.zsh.enable true;
-    enableFishIntegration = lib.mkIf config.programs.fish.enable true;
     enableBashIntegration = lib.mkIf config.programs.bash.enable true;
     enableNushellIntegration = lib.mkIf config.programs.nushell.enable true;
     nix-direnv = {
